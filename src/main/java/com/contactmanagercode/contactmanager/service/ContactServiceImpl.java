2513083e4b395bc89eadc535db7774c01243c194
neorusse <neorusse@gmail.com>
2020-03-03 14:11:07 +0100

    chore: Implement service logic

---
+++ src/main/java/com/contactmanagercode/contactmanager/service/ContactServiceImpl.java
@@ -0,0 +1,47 @@
+package com.contactmanagercode.contactmanager.service;
+
+import com.contactmanagercode.contactmanager.exception.ResourceNotFoundException;
+import com.contactmanagercode.contactmanager.model.Contact;
+import com.contactmanagercode.contactmanager.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+  @Autowired
+  private ContactRepository contactRepository;
+
+
+  @Override
+  public Contact getContact(Integer id) {
+    Optional<Contact> userOptional = contactRepository.findById(id);
+    if (userOptional == null) {
+      throw new ResourceNotFoundException("User with id - " + id + "  not found");
+    }
+    return userOptional.get();
+  }
+
+  @Override
+  public List<Contact> getAllContacts() {
+    return contactRepository.findAll();
+  }
+
+  @Override
+  public void updateContact(Contact contact) {
+    contactRepository.save(contact);
+  }
+
+  @Override
+  public void addContact(String firstName, String lastName, String gender, String phone) {
+    contactRepository.save(new Contact(firstName, lastName, gender, phone));
+  }
+
+  @Override
+  public void saveContact(Contact contact) {
+    contactRepository.save(contact);
+  }
+}
