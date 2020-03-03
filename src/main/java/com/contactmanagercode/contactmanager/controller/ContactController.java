923d050277868a6e2b255d4133a028a132cc4174
neorusse <neorusse@gmail.com>
2020-03-03 14:15:07 +0100

    Refactor routes

--- src/main/java/com/contactmanagercode/contactmanager/controller/ContactController.java
+++ src/main/java/com/contactmanagercode/contactmanager/controller/ContactController.java
@@ -1,22 +1,23 @@
 package com.contactmanagercode.contactmanager.controller;
 
-import com.contactmanagercode.contactmanager.dao.ContactDAO;
-import com.contactmanagercode.contactmanager.exception.DuplicatePhoneException;
 import com.contactmanagercode.contactmanager.model.Contact;
+import com.contactmanagercode.contactmanager.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ContactController {
@@ -24,62 +25,43 @@ public class ContactController {
   private final Logger logger = LoggerFactory.getLogger(ContactController.class);
 
   @Autowired
-  private ContactDAO contactDao;
+  private ContactService contactService;
 
   // Get - Display all contacts
-  @RequestMapping(value="/viewcontacts")
-  public ModelAndView showAllContacts() {
+  @RequestMapping(value="/viewcontacts", method = RequestMethod.GET)
+  public String showAllContacts(ModelMap model) {
 
     logger.info("HTTP GET request received at /viewcontacts url");
 
-    List<Contact> contactlist = contactDao.findAll();
-    return new ModelAndView("viewcontacts","list", contactlist);
+    model.put("contacts", contactService.getAllContacts());
+
+    return "viewcontacts";
   }
 
   // GET - display add contact form
-  @RequestMapping(value="/addcontactform",method= RequestMethod.GET)
-  public String showContacts(ModelMap model) {
+  @RequestMapping(value="/contact", method = RequestMethod.GET)
+  public String showContactPage(Model model) {
 
-    logger.info("HTTP GET request received at /addcontactform url");
+    logger.info("HTTP GET request received at /contact url");
 
-    Contact contact = new Contact();
-    model.addAttribute("contact", contact);
-    return "addcontactform";
+    model.addAttribute("contact", new Contact());
+
+    return "contact";
   }
 
   // POST - save a contact
-  @RequestMapping(value="/savecontact",method=RequestMethod.POST)
-  public ModelAndView saveRegistration(@Valid Contact contact, BindingResult bindingResult) {
+  @RequestMapping(value="/contact", method=RequestMethod.POST)
+  public String saveRegistration(@Valid @ModelAttribute("contact") Contact contact, BindingResult bindingResult, ModelMap model, RedirectAttributes redirectAttributes) {
 
-    logger.info("HTTP POST request received at /savecontact url");
+    logger.info("HTTP POST request received at /contact url");
 
     if(bindingResult.hasErrors()) {
-      return new ModelAndView("addcontactform");
+      return "contact";
     }
 
-    try {
-      contactDao.addOrUpdateContact(contact);
-    } catch (DuplicatePhoneException e) {
-      bindingResult.rejectValue("phone", "", "This phone number already exist");
-      return new ModelAndView("addcontactform");
-    }
+    contactService.saveContact(contact);
 
-    RedirectView redirect = new RedirectView();
-    redirect.setUrl("/viewcontacts");
-
-    return new ModelAndView(redirect);
-
-  }
-
-  // delete a contact
-  @RequestMapping(value="/deletecontact/{id}",method=RequestMethod.GET)
-  public ModelAndView delete(@PathVariable int id) {
-
-    logger.info("HTTP DELETE request received at /deletecontact url");
-
-    Contact contact = contactDao.getOne(id);
-    contactDao.delete(contact);
-    return new ModelAndView("redirect:/viewcontacts");
+    return "redirect:/viewcontacts";
   }
 
 }
